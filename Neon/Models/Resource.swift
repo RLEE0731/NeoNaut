d0dfe29de4a7dcd208aa5d480dd5d8eb3fc369c6
James Donald <jndonald3@gmail.com>
2017-08-18 19:34:43 -0700

    adding a default model

---
+++ Neon/Models/Resource.swift
@@ -0,0 +1,16 @@
+//
+//  Resource.swift
+//  Neon
+//
+//  Created by James Donald on 8/18/17.
+//  Copyright © 2017 CityOfZion. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Resource: Mappable
+{
+    init?(map: Map)
+    { /** Do nothing for now */ }
+}
